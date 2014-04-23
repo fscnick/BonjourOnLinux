634265a01bdd1b7ea21a428ebac17845f1b77d28
Nick Chen <fscnick@hotmail.com>
2014-04-23 08:39:56 +0000

    use user-defined data

--- resolve.c
+++ resolve.c
@@ -6,6 +6,14 @@
 
 #include "dns_sd.h"
 
+struct BrowseReplyData{
+    DNSServiceErrorType errorCode;
+    char *serviceName;
+    char *regType;
+    char *replyDomain;
+    };
+
+
 char *discoveredName;
 char *discoveredRegtype;
 char *discoveredDomain;
@@ -18,6 +26,15 @@ uint16_t endianChange(uint16_t i)
 void customBrowseReply( DNSServiceRef sdRef, DNSServiceFlags flags, uint32_t interfaceIndex, DNSServiceErrorType errorCode,
 			    const char *serviceName, const char *regtype, const char *replyDomain, void *context)
 {
+    // init BrowseReplyData
+    struct BrowseReplyData *replyData=context;
+    replyData->errorCode=errorCode;
+    replyData->serviceName=malloc(strlen(serviceName)+1);
+    replyData->regType=malloc(strlen(regtype)+1);
+    replyData->replyDomain=malloc(strlen(replyDomain)+1);
+    
+    
+    
     printf("In BrowseReply callback!!\n");
     if(flags == kDNSServiceFlagsMoreComing){
 	printf("flag: kDNSServiceFlagsMoreComing\n");
@@ -34,12 +51,15 @@ void customBrowseReply( DNSServiceRef sdRef, DNSServiceFlags flags, uint32_t int
 
     discoveredName=malloc(strlen(serviceName)+1);
     strcpy(discoveredName, serviceName);
+    strcpy(replyData->serviceName, serviceName);
 
     discoveredRegtype=malloc(strlen(regtype)+1);
     strcpy(discoveredRegtype, regtype);
+    strcpy(replyData->regType, regtype);
     
     discoveredDomain=malloc(strlen(replyDomain)+1);
     strcpy(discoveredDomain, replyDomain);
+    strcpy(replyData->replyDomain, replyDomain);
 
 }
 
@@ -80,6 +100,9 @@ int main(int argc, char* argv[])
     DNSServiceRef resolveRef;
     DNSServiceErrorType result;
     DNSServiceFlags nativeFlag=0;
+    
+    struct BrowseReplyData *replyData;
+
 
     int port = 11234;
     char* serviceType="_personal._tcp";
@@ -101,7 +124,7 @@ int main(int argc, char* argv[])
 			serviceType,
 			NULL,	/* domain */
 			&customBrowseReply,	/* callback */
-			NULL	/* context, user-defined that will be an argument of callback */
+			replyData	/* context, user-defined that will be an argument of callback */
 			);
 
     if (result == kDNSServiceErr_NoError){
@@ -121,9 +144,9 @@ int main(int argc, char* argv[])
 	printf("Browse response success!!\n");
 
 	printf("discovered:\n");
-	printf("%s %s %s", discoveredName, discoveredRegtype, discoveredDomain);
+	printf("%s %s %s\n", discoveredName, discoveredRegtype, discoveredDomain);
 
-
+	printf("%s %s %s\n", replyData->serviceName, replyData->regType, replyData->replyDomain);
     }else{
 	printf("Browse response fail!!\n");
 	printf("Error code: %d\n", result);
@@ -171,6 +194,13 @@ int main(int argc, char* argv[])
     // this must be call before calling DNSServiceBrowser again.
     DNSServiceRefDeallocate(browseRef);
     
+    // release replyData
+    free(replyData->serviceName);
+    free(replyData->regType);
+    free(replyData->replyDomain);
+
+
+
     free(discoveredName);
     free(discoveredRegtype);
     free(discoveredDomain);
